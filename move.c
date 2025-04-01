4ed7a301b00c11515ad512bf4e389a70e2607716
hilstadc <hilstadc@my.erau.edu>
2025-04-01 13:08:59 -0700

    Finish move.c

--- move.c
+++ move.c
@@ -6,81 +6,141 @@
 void changeTile(int board[8][8], int tile[2]) {
     if (board[tile[0]][tile[1]] == 1) {
         board[tile[0]][tile[1]] = 2;
-        printf("changed tile %d,%d from white to black",tile[0],tile[1]);
+        printf("changed tile %d,%d from white to black\n",tile[0],tile[1]);
     } else if (board[tile[0]][tile[1]] == 2) {
         board[tile[0]][tile[1]] = 1;
-        printf("changed tile %d,%d from black to white",tile[0],tile[1]);
+        printf("changed tile %d,%d from black to white\n",tile[0],tile[1]);
     } else {
-        printf("ERROR! Attempting to change blank space!");
+        printf("ERROR! Attempting to change blank space!\n");
     }
 }
 
 void move(int board[8][8], int head[2], int tail[2]) {
-    int i, j;
-    if (head[1] == tail[1]) {
-        // up or down
-        if (head[0] > tail[0]) {
-            //up
-            for (i = head[0]-1; i > tail[0]; i--) {
-                changeTile(i,head[1]);
-            }
-        } else {
-            //down
-            for (i = head[0]+1; i < tail[0]; i++) {
-                changeTile(i,head[1]);
-            }
-        }
-    } else if (head[1] > tail[1]) {
-        //left-something
-        if (head[0] > tail[0]) {
-            //up-left
-            i = head[0]-1;
-            for (j = head[1]-1; j > tail[1]; j--) {
-                changeTile(i,j);
-                i--;
-            }
-        } else if (head[0] < tail[0]) {
-            //down-left
-            i = head[0]+1;
-            for (j = head[1]-1; j > tail[1]; j--) {
-                changeTile(i,j);
-                i++;
-            }
-        } else if (head[0] == tail[0]) {
-            //left
-            i = head[0];
-            for (j = head[1]-1; j > tail[1]; j--) {
-                changeTile(i,j);
-            }
-        } else {
-            printf("ERROR: Tile is left but not left?");
-        }
-    } else if (head[0] > tail[0]) {
-        //right-something
-        if (head[0] > tail[0]) {
-            //up-right
-            i = head[0]-1;
-            for (j = head[1]+1; j < tail[1]; j++) {
-                changeTile(i,j);
-                i--;
-            }
-        } else if (head[0] < tail[0]) {
-            //down-right
-            i = head[0]+1;
-            for (j = head[1]+1; j < tail[1]; j++) {
-                changeTile(i,j);
-                i++;
-            }
-        } else if (head[0] == tail[0]) {
-            //right
-            i = head[0];
-            for (j = head[1]+1; j < tail[1]; j++) {
-                changeTile(i,j);
-            }
-        } else {
-            printf("ERROR: Tile is right but not right?");
-        }
+    // check to make sure head (new point) is empty and tail (existing tile) is set
+    if (board[head[0]][head[1]]) {
+        printf("ERROR: Head already occupied!\n");
+    } else if (!board[tail[0]][tail[1]]) {
+        printf("ERROR: Tail not occupied!\n");
     } else {
-        printf("ERROR: tail not in any direction (same tile?)");
+        // set head to the same color as the tail
+        board[head[0]][head[1]] = board[tail[0]][tail[1]];
+
+        //loop variables
+        int i, j;
+        int tileToChange[2];
+        if (head[1] == tail[1]) {
+            // up or down
+            j = head[1];
+            if (head[0] > tail[0]) {
+                //up
+                for (i = head[0]-1; i > tail[0]; i--) {
+                    tileToChange[0] = i;
+                    tileToChange[1] = j;
+                    changeTile(board, tileToChange);
+                }
+            } else if (head[0] < tail[0]) {
+                //down
+                for (i = head[0]+1; i < tail[0]; i++) {
+                    tileToChange[0] = i;
+                    tileToChange[1] = j;
+                    changeTile(board, tileToChange);
+                }
+            } else {
+                printf("ERROR: Tile is in same column but not\n");
+            }
+        } else if (head[1] > tail[1]) {
+            //left-something
+            if (head[0] > tail[0]) {
+                //up-left
+                i = head[0]-1;
+                for (j = head[1]-1; j > tail[1]; j--) {
+                    tileToChange[0] = i;
+                    tileToChange[1] = j;
+                    changeTile(board, tileToChange);
+                    i--;
+                }
+            } else if (head[0] < tail[0]) {
+                //down-left
+                i = head[0]+1;
+                for (j = head[1]-1; j > tail[1]; j--) {
+                    tileToChange[0] = i;
+                    tileToChange[1] = j;
+                    changeTile(board, tileToChange);
+                    i++;
+                }
+            } else if (head[0] == tail[0]) {
+                //left
+                i = head[0];
+                for (j = head[1]-1; j > tail[1]; j--) {
+                    tileToChange[0] = i;
+                    tileToChange[1] = j;
+                    changeTile(board, tileToChange);
+                }
+            } else {
+                printf("ERROR: Tile is left but not left?\n");
+            }
+        } else if (head[1] < tail[1]) {
+            //right-something
+            if (head[0] > tail[0]) {
+                //up-right
+                i = head[0]-1;
+                for (j = head[1]+1; j < tail[1]; j++) {
+                    tileToChange[0] = i;
+                    tileToChange[1] = j;
+                    changeTile(board, tileToChange);
+                    i--;
+                }
+            } else if (head[0] < tail[0]) {
+                //down-right
+                i = head[0]+1;
+                for (j = head[1]+1; j < tail[1]; j++) {
+                    tileToChange[0] = i;
+                    tileToChange[1] = j;
+                    changeTile(board, tileToChange);
+                    i++;
+                }
+            } else if (head[0] == tail[0]) {
+                //right
+                i = head[0];
+                for (j = head[1]+1; j < tail[1]; j++) {
+                    tileToChange[0] = i;
+                    tileToChange[1] = j;
+                    changeTile(board, tileToChange);
+                }
+            } else {
+                printf("ERROR: Tile is right but not right?\n");
+            }
+        } else {
+            printf("ERROR: tail not in any direction (same tile?)\n");
+        }
     }
 }
+
+/*
+// test code
+int main () {
+    int testArr[8][8] = {
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,2,0,0,0,0,0},
+        {0,0,0,1,2,0,0,0},
+        {0,0,0,1,1,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0}};
+    int testHead[2] = {5,5};
+    int testTail[2] = {2,2};
+    move(testArr,testHead,testTail);
+    int i,j;
+    printf("-----------------\n");
+    for(i=0;i<8;i++) {
+        printf("|");
+        for (j=0;j<8;j++) {
+            printf("%d|",testArr[i][j]);
+        }
+        printf("\n");
+        printf("-----------------\n");
+    }
+    while(1) {}
+}
+*/
