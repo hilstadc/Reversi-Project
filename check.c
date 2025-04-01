26b56e679741d40248d93fb5b9695d8056bc3716
adam64iguess <johnsa64@my.erau.edu>
2025-04-01 13:32:40 -0700

    Final changes to check.c

--- check.c
+++ check.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-//UNFINISHED
+#include <stdbool.h>
+//FINISHED
 
 //This function takes and input and the board state, then confrims if the input by the player is a valid move. IF valid, return a 1 and send the inpu to the move function to update board pointer.If invalid, return a 0 and update nothing
 
@@ -13,9 +14,9 @@ bool nullcheck (int cord[2], int row, int col){
     return false; }
   else if (cord[0]==7 && row==1){ //bottom side of board
     return false; }
-  else if (cord[1]==0 && vol==-1){ //left side of 
+  else if (cord[1]==0 && col==-1){ //left side of 
     return false; }
-  else if (cord[1]==7 && int[1]==1){ //right side of the board
+  else if (cord[1]==7 && col==1){ //right side of the board
     return false; }
   else{
     return true;
@@ -26,7 +27,7 @@ bool nullcheck (int cord[2], int row, int col){
 int validate (int board[8][8], int cord [2], int race){ //Main function for here
   int row, col, f, e; //cord[0] = r, cord[1] = c
   bool cool;
-  tail[2]; //where 'move' will end there program;
+  int tail[2]; //where 'move' will end there program;
   cool=false;
   f=1;
   e=2;
@@ -35,23 +36,23 @@ int validate (int board[8][8], int cord [2], int race){ //Main function for here
     e=1;
   }  
   
-  if (board[cord[0],cord[1]] != 0){
-    printf("Cannot place piece on taken spot!");
+  if (board[cord[0]][cord[1]] != 0){
+    printf("Cannot place piece on taken spot!\n");
     return 0;
   }
   
   for (row=-1; row<=1; row++){
-    for (col=-1; row<=1; row++){
+    for (col=-1; col<=1; col++){
       if (row==0 && col==0){ //skip (+0,+0)
         continue; }
       if (nullcheck(cord, row, col) == false){ //nullcheck
         continue; }
     
       if (board[cord[0]+row][cord[1]+col] == e){
-        //                                  HStart loop to validate move
+        //                                  Start loop to validate move
         tail[0]=cord[0];
         tail[1]=cord[1];
-        while (tail==false){
+        while (cool==false){
           tail[0]=tail[0]+row; //move to next piece
           tail[1]=tail[1]+col;
           if (nullcheck(tail, row, col) == false){
@@ -64,10 +65,11 @@ int validate (int board[8][8], int cord [2], int race){ //Main function for here
             cool=true; //hits end, confrims valid move
             tail[0]=tail[0]+row;
             tail[1]=tail[1]+col;
-            //move(board, cord, tail)                            //send input to move function to update board pointer
+            move(board, cord, tail)                            //send input to move function to update board pointer
           }
           else {
-            cool=false//hits enemy piece, keeps going
+            cool=false;//hits enemy piece, keeps going
+            continue;
           }
         }//while loop
         
@@ -81,11 +83,11 @@ int validate (int board[8][8], int cord [2], int race){ //Main function for here
     return 0; //return sends to player
   }
   else{                // VALID
-    //move(board, cord, tail)//
-    return 1; //found something and changed it
+    //move(board, cord, tail)\\
+    return 1;   
   }
-  
+  return 1; //weird ass glitch if return is in here
   
 // failiure == 0              NULL
-// Success == 1              new int[8][8]
+// Success == 1               int[8][8]
 } // give change board state the pointer of the array[][] and both coordinates
