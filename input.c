83a0a26671420c19374d392f91f3b8d5d33370a6
hilstadc <hilstadc@my.erau.edu>
2025-04-02 18:28:03 -0700

    cleared input buffer after scanf

--- input.c
+++ input.c
@@ -18,6 +18,7 @@ int turn (int board[8][8], int player){ // player turn, takes input and calls va
   int col_n;
   int valid = 0;
   int r_var = 0;
+  int c;
   
   print_b(board); // printing current board
   
@@ -31,14 +32,16 @@ int turn (int board[8][8], int player){ // player turn, takes input and calls va
   while (run == 1){ // runs until user inputs valid spot or skips
     printf ("Would you like to: \n(1)make a move \n(2) skip \n");
     scanf ("%d", &skip);
+    while ((c = getchar()) != '\n' && c != EOF) { }
     
     switch (skip){ // player chooses to make a move or skip
     case 1: // player chose to make a move
       printf ("Enter row: ");
       scanf ("%d", &row);
+      while ((c = getchar()) != '\n' && c != EOF) { }
       printf ("Enter column: ");
-      getchar();
       scanf (" %c", &col);
+      while ((c = getchar()) != '\n' && c != EOF) { }
       col = toupper(col); // converting letter to uppercase
       
       switch(col){ // converting letter to number
