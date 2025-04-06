195f2e1ac8323a390aa536602aaeb3fed60c7466
hilstadc <hilstadc@my.erau.edu>
2025-04-06 11:08:12 -0700

    finished highscore.c

--- highscore.c
+++ highscore.c
@@ -3,11 +3,48 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+//#include "highscore.h"
 
-#define MAX_SCORES 5
+
+//for testing
+#define MAX_SCORES 10
+#define NAME_LEN 16
 #define FILE_NAME "highscores.txt"
 
-void read_scores (int scores[], int *count) {
+typedef struct {
+    char name[NAME_LEN];
+    int score;
+} HighScore;
+
+
+void print_scores (HighScore scores[], int count) {
+    if (count == 0) {
+        printf("No high scores yet.\n");
+        return;
+    }
+
+    int i;
+    for (i = 0; i < count; i++) {
+        printf("%2d. %-15s %2d\n", i + 1, scores[i].name, scores[i].score);
+    }
+}
+
+void sort_scores (HighScore scores[], int count) {
+    //bubble sort
+    int i, j;
+    for (i = 0; i < count - 1; i++) {
+        for (j = 0; j < count - i - 1; j++) {
+            if (scores[j].score < scores[j + 1].score) {
+                HighScore temp = scores[j];
+                scores[j] = scores[j + 1];
+                scores[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void read_scores (HighScore scores[], int *count) {
     //open highscore file for reading
     FILE* fptr;
     fptr = fopen(FILE_NAME, "r");
@@ -15,13 +52,12 @@ void read_scores (int scores[], int *count) {
     //if can't read, assume doesn't exist
     if (!fptr) {
         *count = 0;
-        printf("No esiting highscores found!");
         return;
     }
 
     //check if data is on each line and store it in scores
     //stop when either no more data or when max is reached
-    while (fscanf(fptr, "%d", &scores[*count]) == 1 && *count < MAX_SCORES) {
+    while (fscanf(fptr, "%s %d", scores[*count].name, &scores[*count].score) == 2 && *count < MAX_SCORES) {
         (*count)++;
     }
 
@@ -29,7 +65,7 @@ void read_scores (int scores[], int *count) {
     fclose(fptr);
 }
 
-void write_scores (int scores[], int count) {
+void write_scores (HighScore scores[], int count) {
     //open highscore file for writing
     FILE* fptr;
     fptr = fopen(FILE_NAME, "w");
@@ -42,39 +78,63 @@ void write_scores (int scores[], int count) {
 
     //store each value in scores up to the number of scores
     int i;
-    for (i = 0; i < count; i++) {
-        fprintf(fptr, "%d\n", scores[i]);
+    for (int i = 0; i < count; i++) {
+        fprintf(fptr, "%s %d\n", scores[i].name, scores[i].score);
     }
 
     //close file
     fclose(fptr);
 }
 
-void check_scores (int new_score) {
+void check_scores (int new_score, int color) {
     //add one for new score
-    int scores[MAX_SCORES + 1];
+    HighScore scores[MAX_SCORES + 1];
     //number of scores in file
     int count = 0;
 
-    //store the existing scores in a variable with the amount
+    //store existing scores in highscore array
     read_scores(scores, &count);
-    //add the new score to the end of the existing scores
-    scores[count++] = new_score;
 
-    //bubble sort
-    int i, j;
-    for (i = 0; i < count - 1; i++) {
-        for (j = 0; j < count - i - 1; j++) {
-            if (scores[j] < scores[j + 1]) {
-                int temp = scores[j];
-                scores[j] = scores[j + 1];
-                scores[j + 1] = temp;
-            }
+    //print leaderboard before checking
+    printf("\nCurrent Leaderboard:\n");
+    print_scores(scores, count);
+
+    if (count < MAX_SCORES || new_score > scores[count - 1].score) {
+        //get player's name
+        char name[NAME_LEN];
+        if (color == 1) {
+            printf("\nCongratulations! White got a high score!\n");
+        } else {
+            printf("\nCongratulations! Black got a high score!\n");
         }
-    }
+        printf("Enter your name (max 15 characters): ");
+        scanf("%15s", name);
+        if (strlen(name) >= NAME_LEN - 1)
+            printf("Name truncated to 15 characters.\n");
 
-    // reset count to 10 if max scores is full
-    if (count > MAX_SCORES) count = MAX_SCORES;
-    // save sorted scores
-    saveScores(scores, count);
+        //add new score and sort
+        strncpy(scores[count].name, name, NAME_LEN);
+        scores[count].score = new_score;
+        count++;
+
+        sort_scores(scores, count);
+
+        if (count > MAX_SCORES) count = MAX_SCORES;
+
+        write_scores(scores, count);
+
+        printf("\nUpdated Leaderboard:\n");
+        print_scores(scores, count);
+    }
+}
+
+//test code
+int main () {
+    int score = 30;
+
+    check_scores(score, 1);
+
+    while(1);
+
+    return 0;
 }
--- highscore.h
+++ highscore.h
@@ -1,9 +1,20 @@
-#ifndef INPUT_H_
-#define INPUT_H_
+#ifndef HIGHSCORE_H_
+#define HIGHSCORE_H_
+
+#define MAX_SCORES 10
+#define NAME_LEN 16
+#define FILE_NAME "highscores.txt"
+
+typedef struct {
+    char name[NAME_LEN];
+    int score;
+} HighScore;
 
 //function declarations
-void read_scores (int scores[], int *count);
-void write_scores (int scores[], int count);
-void check_scores (int new_score);
+void print_scores (HighScore scores[], int count);
+void sort_scores (HighScore scores[], int count);
+void read_scores (HighScore scores[], int *count);
+void write_scores (HighScore scores[], int count);
+void check_scores (int new_score, int color);
 
 #endif
